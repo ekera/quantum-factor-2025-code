b89425bed0e45ac5822d129f5e6842b43af12508
Martin Ekerå <ekera@kth.se>
2025-08-11 00:57:15 +0200

    Updated the plot script

--- src/facto/algorithm/estimates/main2_generate_pareto_plot.py
+++ src/facto/algorithm/estimates/main2_generate_pareto_plot.py
@@ -11,19 +11,61 @@ from facto.algorithm.estimates.main1_generate_cost_table_data import read_bucket
 
 def save_cost_figure(*, csv_path: str | pathlib.Path, out_path: str | pathlib.Path | None, title: str | None = None, show: bool = False):
     buckets = read_buckets(csv_path)
-    if title == "RSA IFP": # Note: Temporary solution.
-      gid_costs = {
-          1024: ([400_000_000], [3*1024 + math.ceil(0.002*1024*math.log2(1024))]),
-          2048: ([2_700_000_000], [3*2048 + math.ceil(0.002*2048*math.log2(2048))]),
-          3072: ([9_900_000_000], [3*3072 + math.ceil(0.002*3072*math.log2(3072))]),
-      }
-      chev_costs = {
-          2048: ([2**40.87], [1730]),
-          3072: ([2**42.7], [2415]),
-          4096: ([2**43.49], [3096]),
-          6144: ([2**45.36], [4430]),
-          8192: ([2**46.63], [5781]),
-      }
+
+    # Comparisons with other estimates.
+    ge21_costs = None;
+    cfs24_costs = None;
+
+    def ge21_cost(n, n_e):
+        return ([0.2 * n_e * (n ** 2) + 0.0003 * n_e * (n ** 2) * math.log2(n)],
+                [3 * n + math.ceil(0.002 * n * math.log2(n))]);
+
+    def ge21_cost_rsa(n):
+        m = n / 2 - 1;
+        l = m - 20;
+        return ge21_cost(n, n_e = m + 2 * l);
+
+    nist_model = {
+      512:   56,
+      768:   72,
+      1024:  80,
+      2048: 112,
+      3072: 128,
+      4096: 152,
+      6144: 176,
+      8192: 200
+    }
+
+    if title == "RSA IFP": # Note: Temporary solution to use the title.
+        ge21_costs = {
+            n: ge21_cost_rsa(n) for n in [1024, 2048, 3072, 4096]
+        }
+
+        cfs24_costs = {
+            2048: ([2**40.87], [1730]),
+            3072: ([2**42.70], [2415]),
+            4096: ([2**43.49], [3096]),
+            6144: ([2**45.36], [4430]),
+            8192: ([2**46.63], [5781]),
+        }
+    elif title == "Short DLP in safe-prime groups":
+        ge21_costs = {
+            n: ge21_cost(n, 3 * 2 * nist_model[n]) for n in [1024, 2048, 3072, 4096]
+        }
+
+        cfs24_costs = {
+            2048: ([2**36.47], [684]),
+            3072: ([2**37.42], [719]),
+            4096: ([2**38.30], [774]),
+            6144: ([2**39.28], [831]),
+            8192: ([2**40.05], [885]),
+        }
+    elif title == "DLP in Schnorr groups of known order":
+        ge21_costs = {
+            n: ge21_cost(n, 2 * (2 * nist_model[n] + 5)) for n in [1024, 2048, 3072, 4096]
+        }
+
+    # Plot.
     k = 0
     fig: plt.Figure
     ax: plt.Axes
@@ -50,29 +92,38 @@ def save_cost_figure(*, csv_path: str | pathlib.Path, out_path: str | pathlib.Pa
         ys = ys[::-1]
         xs = xs[::-1]
 
-        ax.text(xs[0], ys[0], f'{n=}', horizontalalignment='left', verticalalignment='top', color=f'C{k}', fontsize=12)
-        ax.plot(xs, ys, label=f'{n=}', zorder=100, marker='.', color=f'C{k}')
-        if title == "RSA IFP": # Note: Temporary solution.
-          if n in gid_costs:
-              xs2, ys2 = gid_costs[n]
+        ax.text(xs[0], ys[0], f'  $n={n}$', horizontalalignment='left', verticalalignment='top', fontsize=8)
+        ax.plot(xs, ys, label=f'$n={n}$', zorder=100, marker='.', color=f'C{k}')
+
+        if None != ge21_costs:
+          if n in ge21_costs:
+              xs2, ys2 = ge21_costs[n]
               ax.plot(xs2, ys2, zorder=100, marker='s', color=f'C{k}')
-              ax.text(xs2[0], ys2[0], f'GE21:n={n}', horizontalalignment='left', verticalalignment='top', color=f'C{k}', fontsize=12)
-          if n in chev_costs:
-              xs2, ys2 = chev_costs[n]
+              ax.text(xs2[0], ys2[0], f'  [GE21]\n  $n={n}$', horizontalalignment='left', verticalalignment='top', fontsize=8)
+
+        if None != cfs24_costs:
+          if n in cfs24_costs:
+              xs2, ys2 = cfs24_costs[n]
               ax.plot(xs2, ys2, zorder=100, marker='p', color=f'C{k}')
-              ax.text(xs2[0], ys2[0], f'CFS24:n={n}', horizontalalignment='left', verticalalignment='top', color=f'C{k}', fontsize=12)
+              ax.text(xs2[0], ys2[0], f'  [CFS24]\n  $n={n}$', horizontalalignment='left', verticalalignment='top', fontsize=8)
+
         k += 1
 
     ax.loglog()
-    ax.set_ylim(10**2, 10**4)
-    ax.set_xlim(10**7, 10**15)
-    ax.grid(zorder=1)
+
+    ax.set_ylim(2 * 10**2, 1.5 * 10**4)
+    ax.set_xlim(10**7, 3 * 10**14)
+
+    plt.grid(True, which='major', linestyle='-', linewidth=0.5, color='gray')
+    plt.grid(True, which='minor', linestyle=':', linewidth=0.3, color='gray')
+
     if None != title:
-        ax.set_title(title, fontsize=16)
-    ax.set_xlabel(f'Toffoli Gates', fontsize=16)
-    ax.set_ylabel(fr'$\mathbf{{Logical}}$ Qubits', fontsize=16)
+        ax.set_title(title, fontsize=13)
+    ax.set_xlabel(f'Toffoli gates (overall summed over all runs required)', fontsize=13)
+    ax.set_ylabel(fr'Logical qubits', fontsize=13)
+    ax.legend(loc='lower right')
     fig.set_dpi(200)
-    fig.set_size_inches(12, 7)
+    fig.set_size_inches(15, 7)
     fig.tight_layout()
     if out_path is not None:
         fig.savefig(out_path)
