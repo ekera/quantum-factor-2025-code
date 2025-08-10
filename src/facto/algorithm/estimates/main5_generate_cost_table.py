735086482076909979bdba6c4403e9f0e748cec6
Martin Ekerå <ekera@kth.se>
2025-08-11 01:01:40 +0200

    Updated the table generation code to output partial Latex tables

--- src/facto/algorithm/estimates/main5_generate_cost_table.py
+++ src/facto/algorithm/estimates/main5_generate_cost_table.py
@@ -6,31 +6,16 @@ from typing import Any, Callable
 
 from facto.algorithm.estimates._cost_config import CostConfig
 from facto.algorithm.estimates.main1_generate_cost_table_data import read_buckets
-from facto.algorithm.prep import table_str
-
 
 def generate_cost_table(*, csv_path: str | pathlib.Path, score_func: Callable[[int, int], float], out_path: str | pathlib.Path | None = None):
     buckets = read_buckets(csv_path)
 
-    k = 0
-    entries = {}
-    entries[-1 - 2j] = '$n$'
-    entries[0 - 2j] = '$s$'
-    entries[1 - 2j] = r'$\ell$'
-    entries[2 - 2j] = '$w_1$'
-    entries[3 - 2j] = '$w_3$'
-    entries[4 - 2j] = '$w_4$'
-    entries[5 - 2j] = '$f$'
-    entries[6 - 2j] = r'$m$'
-    entries[7 - 2j] = r'$P_{\text{deviant}}$'
-    entries[8 - 2j] = r'E(shots)'
-    entries[9 - 2j] = r'Toffolis'
-    entries[10- 2j] = r'Qubits'
-    for k2 in range(-1, 11):
-        entries[k2 - 1j] = '---'
-    for n, v in sorted(buckets.items()):
+    table_data = ""
+  
+    for _, v in sorted(buckets.items()):
         tup = min(v.items(), key=lambda e: score_func(e[0], e[1]["tofs"]))
         result = tup[1]
+
         att = CostConfig.from_params(
             modulus_bitlength=result["modulus_bitlength"],
             num_input_qubits=result["num_input_qubits"],
@@ -43,38 +28,71 @@ def generate_cost_table(*, csv_path: str | pathlib.Path, score_func: Callable[[i
             len_acc=result["len_acc"])
 
         details = result["details"]
-        if "s" in details.keys():
-            s = int(details["s"])
+
+        # n, z
+        table_data += str(result["modulus_bitlength"]) + " & "
+        if "z" in details.keys():
+            table_data += str(details["z"]) + " & "
         else:
             # Not relevant/available for this parameterization.
-            s = "--"
+            table_data += "---" + "  &  "
 
-        tof_str = f'{att.toffolis:0.2g}'
+        # m_A, s_A, l_A, [sigma_a], P_pp, n_A
+        table_data += str(details["m"]) + " & "
+        if "s" in details.keys():
+            table_data += str(details["s"]) + " & "
+        else:
+            # Not relevant/available for this parameterization.
+            table_data += "---" + " & "
+        table_data += str(details["l"]) + " & "
+        if "sigma" in details.keys():
+          table_data += str(details["sigma"]) + " & "
+
+        P_pp = result["pp_success_probability"]
+        P_fail = 1 - P_pp
+        table_data += f'{P_fail:.0e}' + "  &  "
+
+        table_data += str(result["num_shots"]) + "  &  "
+
+        # m, \ell, w_1, w_3, w_4, f, P_\text{deviate}
+        table_data += str(result["num_input_qubits"]) + "  &  "
+
+        table_data += str(result["l"]) + " & "
+        table_data += str(result["w1"]) + " & "
+        table_data += str(result["w3"]) + " & "
+        table_data += str(result["w4"]) + " & "
+        table_data += str(result["len_acc"]) + " & "
+
+        P_dev = f'{att.conf.probability_of_deviation_failure:0.2%}'
+        P_dev = P_dev.replace('%', '\\%')
+        table_data += P_dev + "  &  "
+
+        # E(shots)
+        exp_shots = att.conf.expected_shots
+        table_data += f'{exp_shots:0.1f}' + "  &  "
+
+        # Toffolis (per shot), Toffolis (overall), Qubits
+        tof_overall = att.toffolis
+        tof_per_shot = att.toffolis / exp_shots
+
+        tof_str = f'{tof_per_shot:0.2g}'
         if '.' not in tof_str:
             tof_str = tof_str.replace('e', '.0e')
-        entries[-1 + k*1j] = att.conf.modulus.bit_length()
-        entries[0 + k*1j] = s
-        entries[1 + k*1j] = att.conf.rns_primes_bit_length
-        entries[2 + k*1j] = att.conf.window1
-        entries[3 + k*1j] = att.conf.window3a
-        entries[4 + k*1j] = att.conf.window4
-        entries[5 + k*1j] = att.conf.len_accumulator
-        entries[6 + k*1j] = att.conf.num_input_qubits
-        entries[7 + k*1j] = f'{att.conf.probability_of_deviation_failure:0.2%}'.replace('%', '\\%')
-        entries[8 + k*1j] = f'{att.conf.expected_shots:0.1f}'
-        entries[9 + k*1j] = tof_str
-        entries[10+ k*1j] = att.conf.estimated_logical_qubits
+        table_data += str(tof_str) + " & "
 
-        k += 1
+        tof_str = f'{tof_overall:0.2g}'
+        if '.' not in tof_str:
+            tof_str = tof_str.replace('e', '.0e')
+        table_data += tof_str + " & "
+
+        table_data += str(att.conf.estimated_logical_qubits) + " \\\\\n"
+
     if out_path is not None:
         f: Any
         with open(out_path, 'w') as f:
-            print(table_str(
-                entries,
-                latex=True,
-            ), file=f)
+            print(table_data, file=f)
     else:
-        print(table_str(entries))
+        print(table_data)
 
     if out_path is not None:
         print(f'wrote file://{pathlib.Path(out_path).absolute()}')
